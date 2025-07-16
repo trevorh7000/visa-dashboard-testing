3c65048ac2e38a532eb3b52547f4b423f68dd9cb
Trevor <tghughes@gmail.com>
2025-07-16 12:28:05 +0200

    Auto update from processor script @ 2025-07-16 12:28:05

--- visa-dashboard-web/dashboard.py
+++ visa-dashboard-web/dashboard.py
@@ -30,7 +30,7 @@ st.markdown("""
 
 @st.cache_data
 def load_data():
-# cache-bust: 2025-07-16T12:16:17.362064
+# cache-bust: 2025-07-16T12:28:05.072319
     import os
     BASE_DIR = os.path.dirname(__file__)
     db_path = os.path.join(BASE_DIR, "decisions.db")
